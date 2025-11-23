2c163ad872fdc2e86cc459c0698a17c5421e7b61
vinay <108125015+vinaymolathoti@users.noreply.github.com>
2025-11-23 19:31:46 +0530

    Fix BASE_URL & params for stats

--- app/code/[code]/page.js
+++ app/code/[code]/page.js
@@ -2,7 +2,7 @@ export const dynamic = "force-dynamic";
 
 async function getStats(code) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/links/${code}`, {
-    cache: "no-store"
+    cache: "no-store",
   });
 
   if (!res.ok) return null;
